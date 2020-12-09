136add72fe9ea2c4879438f0f4c5b70eb89b6d1c
Paresh Chouhan <pareshchouhan2013@gmail.com>
2020-12-09 12:02:25 +0530

    Added API_URL instead

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://https://clipboard-health-project-ten.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://clipboard-health-project-ten.vercel.app';
