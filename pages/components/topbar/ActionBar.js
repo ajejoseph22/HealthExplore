b5a54002f8e8c18128c42be3b5ee7c4bdd2dedf2
bigdreamer07 <lancerstar2020@outlook.com>
2020-12-12 20:03:28 +0800

    more familar colors for top bar

--- pages/components/topbar/ActionBar.js
+++ pages/components/topbar/ActionBar.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 const ActionBar = props => {
   return (
     <div className="flex space-x-6 items-center">
-      <a href="#" className="hidden lg:flex border rounded-md border-blue-600 pl-1.5 pr-1.5 pt-2 pb-2">
-        <span className="text-center text-blue-600 font-semibold">CREATE JOB</span>
+      <a href="#" className="hidden lg:flex border rounded-md border-blue-500 pl-1.5 pr-1.5 pt-2 pb-2">
+        <span className="text-center text-blue-500 font-semibold">CREATE JOB</span>
       </a>
       <a href="#" className="inline-block relative">
-        <div className="h-10 w-10 rounded-full bg-blue-600 border border-white flex items-center justify-center">
+        <div className="h-10 w-10 rounded-full bg-blue-500 border border-white flex items-center justify-center">
           <span className="text-white text-sm">Jo</span>  
         </div>
         <span className="absolute top-0 right-0 h-4 w-4 rounded-full bg-red-600 border border-white flex items-center justify-center">
