ad11499ab01b8f76829fe79cd08c2e64801f73d7
Joseph Aje <ajejoseph22@gmail.com>
2020-12-22 09:42:04 +0100

    leveraged context API for neater data flow

--- pages/index.js
+++ pages/index.js
@@ -6,36 +6,51 @@ import SearchBar from "../components/searchbar";
 import Main from "../components/main";
 import { getAllJobs } from "../lib/jobs";
 
-export async function getServerSideProps() {
-  const response = await fetch(`http://localhost:3000/api/jobs`);
-  const jobs = (await response.json()).result;
+//////// CONTEXT /////////
+export const HomeContext = React.createContext({
+  setFilter: () => {},
+});
 
-  return {
-    props: {
-      jobs,
-    },
-  };
-}
-
-export default function Home(props) {
+const Home = (props) => {
   const [query, setQuery] = React.useState("");
-  React.useEffect(() => {
-    console.log("changed", query);
-  }, [query]);
+  const [filters, setFilters] = React.useState({});
+
   const handleSetQuery = (query) => {
     setQuery(query);
   };
-  return (
-    <div>
-      <Head>
-        <title>Health Explore</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <Navbar />
-      <SearchBar setQuery={handleSetQuery} />
-      <Main query={query} {...props} />
-      <footer></footer>
-    </div>
+  const handleSetFilters = (filtersObj) => {
+    setFilters({ ...filters, ...filtersObj });
+  };
+
+  return (
+    <HomeContext.Provider
+      value={{ setFilter: (filterObj) => handleSetFilters(filterObj) }}
+    >
+      <div>
+        <Head>
+          <title>Health Explore</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Navbar />
+        <SearchBar setFilters={handleSetFilters} setQuery={handleSetQuery} />
+        <Main filters={filters} query={query} {...props} />
+        <footer />
+      </div>
+    </HomeContext.Provider>
   );
-}
+};
+
+// export async function getServerSideProps() {
+//   const response = await fetch(`http://localhost:3000/api/jobs`);
+//   const jobs = (await response.json()).result;
+//   console.log("got here");
+//   return {
+//     props: {
+//       jobs,
+//     },
+//   };
+// }
+
+export default Home;
