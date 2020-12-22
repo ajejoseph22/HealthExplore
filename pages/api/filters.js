ad727a7bbe820f2bbb42c38ef666b6a6c16b3268
Joseph Aje <ajejoseph22@gmail.com>
2020-12-22 04:42:26 +0100

    added filter api

--- pages/api/filters.js
+++ pages/api/filters.js
@@ -1,7 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import filters from '../../data/filters';
+import filters from "../../data/filters";
+import sort from "fast-sort";
 
 export default async (req, res) => {
+  const { payload, criteria } = req.body;
+  console.log(payload, payload);
+  const sortParamsMap = {
+    location: "county",
+    role: "job_title",
+  };
+
+  const result = sort(payload).by(
+    Object.keys(criteria).reduce((acc, key) => {
+      if (criteria[key]) {
+        acc.push({ [criteria[key]]: (job) => job[sortParamsMap[key]] });
+      }
+
+      return acc;
+    }, [])
+  );
+
   res.statusCode = 200;
-  res.json(filters)
-}
+  res.json({ filteredResult: result });
+};
